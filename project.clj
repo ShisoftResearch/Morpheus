ddde28e9852b2ed8e60b79d17196c53987344db8
Jack Shi <shisoftgenius@gmail.com>
2016-02-21 21:07:27 +0800

    project deps

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [neb "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot morpheus.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :jvm-opts [;"-Djava.rmi.server.hostname=<hostname>" ;;add this when remote-connect fail
+             ;"-Dcom.sun.management.jmxremote"
+             ;"-Dcom.sun.management.jmxremote.port=9876"
+             ;"-Dcom.sun.management.jmxremote.authenticate=false"
+             ;"-Dcom.sun.management.jmxremote.ssl=false"
+             "-Xmx8g"
+             "-XX:+UseParNewGC" "-XX:+UseConcMarkSweepGC" "-XX:+CMSParallelRemarkEnabled"])
