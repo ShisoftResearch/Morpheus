ca6fd8d6ce414025610bbca8830fb19510abcbbc
Jack Shi <shisoftgenius@gmail.com>
2016-02-21 19:17:05 +0800

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to morpheus
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/morpheus/core.clj
@@ -0,0 +1,7 @@
+(ns morpheus.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
