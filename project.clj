68c8fffdeed9d473bdaf525ce1f4f0c09dcc10f5
John Shi <shisoftgenius@gmail.com>
2016-04-06 00:50:19 +0800

    adjust memory for testing

--- project.clj
+++ project.clj
@@ -20,6 +20,6 @@
              "-Dcom.sun.management.jmxremote.port=9876"
              "-Dcom.sun.management.jmxremote.authenticate=false"
              "-Dcom.sun.management.jmxremote.ssl=false"
-             "-Xmx6G"
+             "-Xmx8G"
              ;"-XX:+UseParNewGC" "-XX:+UseConcMarkSweepGC" "-XX:+CMSParallelRemarkEnabled"
              ])
--- src/clj/morpheus/integration/sources/wikidata.clj
+++ src/clj/morpheus/integration/sources/wikidata.clj
@@ -224,7 +224,7 @@
                   :port 5124
                   :zk  "10.0.1.104:2181"
                   :trunks-size "5gb"
-                  :memory-size "25gb"
+                  :memory-size "50gb"
                   :schema-file "configures/neb-schemas.edn"
                   :data-path   "wikidata"
                   :durability true
