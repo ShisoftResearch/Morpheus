5c622363fba97c96f8f52d63865b4c851affdcdb
John Shi <shisoftgenius@gmail.com>
2016-04-30 11:55:29 +0800

    increase heap size for test

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
              "-Dcom.sun.management.jmxremote.port=9876"
              "-Dcom.sun.management.jmxremote.authenticate=false"
              "-Dcom.sun.management.jmxremote.ssl=false"
-             "-Xmx4G"
+             "-Xmx16G"
              "-enableassertions"
              "-XX:+UseParNewGC" "-XX:+UseConcMarkSweepGC" "-XX:+CMSParallelRemarkEnabled"
              ])
