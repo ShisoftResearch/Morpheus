42e51e09b8580956cc003b189dc9769628143c0d
Jack Shi <shisoftgenius@gmail.com>
2016-05-26 05:14:01 +0800

    adjacent list

--- src/clj/morpheus/traversal/dfs/mutable.clj
+++ src/clj/morpheus/traversal/dfs/mutable.clj
@@ -1,8 +1,2 @@
 (ns morpheus.traversal.dfs.mutable)
 
-(defn new-mutable-vertex [vertex]
-  {:vertex vertex
-   :edges (atom [])})
-
-(defn push-adj [mutable-vertex target-mutable-vertex-id edge]
-  (swap! (:edges mutable-vertex) conj {:edge edge :vertex target-mutable-vertex-id}))
--- src/clj/morpheus/traversal/dfs/rebuild.clj
+++ src/clj/morpheus/traversal/dfs/rebuild.clj
@@ -1,7 +1,30 @@
-(ns morpheus.traversal.dfs.rebuild)
+(ns morpheus.traversal.dfs.rebuild
+  (:require [morpheus.traversal.dfs.mutable :as mv]))
+
+(defn new-mutable-vertex [vp]
+  {:vertex-props vp
+   :adjacents    (atom [])})
+
+(defn push-adj [mutable-vertex target-mutable-vertex-props]
+  (swap! (:adjacents mutable-vertex) conj target-mutable-vertex-props))
+
+(defn preproc-stack-for-adj-list [stack]
+  (into
+    {}
+    (map
+      (fn [{:keys [id] :as vp}]
+        [id (new-mutable-vertex vp)])
+      stack)))
 
 (defn adjacancy-list [stack]
-  )
+  (let [vertices-map (preproc-stack-for-adj-list stack)]
+    (doseq [[vid vm] vertices-map]
+      (let [vp (:vertex-props vm)
+            {:keys [parent edge]} vp]
+        (push-adj (get vertices-map parent) vp)))
+    (map (fn [[vid vm]]
+           (update vm :adjacents deref))
+         vertices-map)))
 
 (defn paths-from-stack [stack a b]
   )
