804161488465940440a9c318bf8babbd6979f09c
Jack Shi <shisoftgenius@gmail.com>
2016-06-03 00:35:34 +0800

    update objects

--- src/clj/morpheus/query/lang/update.clj
+++ src/clj/morpheus/query/lang/update.clj
@@ -4,7 +4,7 @@
             [morpheus.models.edge.core :as edge]
             [neb.utils :refer [map-on-keys]]))
 
-(defn update-vertex* [vertex field-exp-map]
+(defn update-object* [vertex field-exp-map]
   (loop [exp-pairs field-exp-map
          vertex-to-update vertex]
     (if (empty? exp-pairs)
@@ -14,8 +14,13 @@
                (assoc-in vertex-to-update k
                          (AST/eval-with-data vertex exp)))))))
 
-(defn update-vertex [vertex field-exp-map]
-  (vertex/update-vertex!
-    vertex
-    'morpheus.query.lang.update/update-vertex*
+(defn update-object [update-func obj field-exp-map]
+  (update-func
+    obj 'morpheus.query.lang.update/update-object*
     (map-on-keys AST/parse-map-path field-exp-map)))
+
+(defn update-vertex [vertex field-exp-map]
+  (update-object vertex/update-vertex! vertex field-exp-map))
+
+(defn update-edge [edge field-exp-map]
+  (update-object edge/update-edge! edge field-exp-map))
