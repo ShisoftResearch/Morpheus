c3f548e01bba709886e425824bc146337319f55f
Jack Shi <shisoftgenius@gmail.com>
2017-05-30 19:49:37 +0800

    neb client

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,15 +1,32 @@
 use std::sync::Arc;
+use neb::client::{Client as NebClient, NebClientError};
 use neb::server::{ServerOptions, NebServer, ServerError};
 
+pub enum MorpheusServerErrors {
+    ServerError(ServerError),
+    ClientError(NebClientError)
+}
+
 pub struct MorpheusServer {
-    neb_server: Arc<NebServer>
+    neb_server: Arc<NebServer>,
+    neb_client: Arc<NebClient>
 }
 
 impl MorpheusServer {
-    pub fn new(opts: &ServerOptions) -> Result<Arc<MorpheusServer>, ServerError> {
-        let neb_server = NebServer::new(opts)?;
+    pub fn new(opts: &ServerOptions) -> Result<Arc<MorpheusServer>, MorpheusServerErrors> {
+        let neb_server = match NebServer::new(opts) {
+            Ok(server) => server,
+            Err(e) => return Err(MorpheusServerErrors::ServerError(e))
+        };
+        let neb_client = match NebClient::new(
+            &neb_server.rpc, &opts.meta_members,
+            &opts.group_name) {
+            Ok(client) => client,
+            Err(e) => return Err(MorpheusServerErrors::ClientError(e))
+        };
         Ok(Arc::new(MorpheusServer {
-            neb_server: neb_server
+            neb_server: neb_server,
+            neb_client: Arc::new(neb_client),
         }))
     }
 }
