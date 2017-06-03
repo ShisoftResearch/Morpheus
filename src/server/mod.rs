7490319ac8f75516a0c613b0f7020a557df77ce5
Jack Shi <shisoftgenius@gmail.com>
2017-06-04 03:49:24 +0800

    link schema service with server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use bifrost::raft::state_machine::master::ExecError;
 use neb::client::{Client as NebClient, NebClientError};
 use neb::server::{ServerOptions, NebServer, ServerError};
 
@@ -6,9 +7,10 @@ pub mod general;
 pub mod schema;
 pub mod traversal;
 
-pub enum MorpheusServerErrors {
+pub enum MorpheusServerError {
     ServerError(ServerError),
-    ClientError(NebClientError)
+    ClientError(NebClientError),
+    InitSchemaError(ExecError)
 }
 
 pub struct MorpheusServer {
@@ -18,16 +20,27 @@ pub struct MorpheusServer {
 
 impl MorpheusServer {
     pub fn new(opts: &ServerOptions)
-        -> Result<Arc<MorpheusServer>, MorpheusServerErrors> {
+        -> Result<Arc<MorpheusServer>, MorpheusServerError> {
         let neb_server = match NebServer::new(opts) {
             Ok(server) => server,
-            Err(e) => return Err(MorpheusServerErrors::ServerError(e))
+            Err(e) => return Err(MorpheusServerError::ServerError(e))
         };
         let neb_client = match NebClient::new(
             &neb_server.rpc, &opts.meta_members,
             &opts.group_name) {
             Ok(client) => Arc::new(client),
-            Err(e) => return Err(MorpheusServerErrors::ClientError(e))
+            Err(e) => return Err(MorpheusServerError::ClientError(e))
+        };
+        if opts.is_meta {
+            if let &Some(ref raft_service) = &neb_server.raft_service {
+                schema::SchemaContainer::new_meta_service(raft_service);
+            } else {
+                panic!("raft service should be ready for meta server");
+            }
+        }
+        let schema_container = match schema::SchemaContainer::new_client(&neb_client.raft_client) {
+            Ok(container) => container,
+            Err(e) => return Err(MorpheusServerError::InitSchemaError(e))
         };
         Ok(Arc::new(MorpheusServer {
             neb_server: neb_server,
