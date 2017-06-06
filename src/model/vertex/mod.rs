18beffdb353835bfddb345f04dd4a4e4d6157f4e
Jack Shi <shisoftgenius@gmail.com>
2017-06-06 23:48:59 +0800

    factory mode?

--- src/model/vertex/mod.rs
+++ src/model/vertex/mod.rs
@@ -1,5 +1,9 @@
 use neb::ram::schema::Field;
 use neb::ram::types::TypeId;
+use neb::ram::cell::Cell;
+use neb::client::{Client as NebClient};
+use server::schema::{MorpheusSchema, SchemaType, SchemaContainer};
+use std::sync::Arc;
 
 lazy_static! {
     pub static ref VERTEX_TEMPLATE: Vec<Field> = vec![
@@ -7,4 +11,9 @@ lazy_static! {
             Field::new(&String::from("_outbound"), TypeId::Id as u32, true, false, None),
             Field::new(&String::from("_indirected"), TypeId::Id as u32, true, false, None)
         ];
+}
+
+pub struct VertexFactory {
+    schema: Arc<SchemaContainer>,
+    neb_client: Arc<NebClient>
 }
