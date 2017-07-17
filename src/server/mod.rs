a878859cba06ff6cd5b11a7a394fd9e26704f3ef
Jack Shi <shisoftgenius@gmail.com>
2017-07-18 02:49:47 +0800

    concise error result

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -25,17 +25,11 @@ pub struct MorpheusServer {
 
 impl MorpheusServer {
     pub fn new(opts: &ServerOptions)
-        -> Result<Arc<MorpheusServer>, MorpheusServerError> {
-        let neb_server = match NebServer::new(opts) {
-            Ok(server) => server,
-            Err(e) => return Err(MorpheusServerError::ServerError(e))
-        };
-        let neb_client = match NebClient::new(
+               -> Result<Arc<MorpheusServer>, MorpheusServerError> {
+        let neb_server = NebServer::new(opts).map_err(MorpheusServerError::ServerError)?;
+        let neb_client = Arc::new(NebClient::new(
             &neb_server.rpc, &opts.meta_members,
-            &opts.group_name) {
-            Ok(client) => Arc::new(client),
-            Err(e) => return Err(MorpheusServerError::ClientError(e))
-        };
+            &opts.group_name).map_err(MorpheusServerError::ClientError)?);
         if opts.is_meta {
             if let &Some(ref raft_service) = &neb_server.raft_service {
                 schema::SchemaContainer::new_meta_service(raft_service);
@@ -43,12 +37,9 @@ impl MorpheusServer {
                 panic!("raft service should be ready for meta server");
             }
         }
-        let schema_container = match schema::SchemaContainer::new_client(
+        let schema_container = schema::SchemaContainer::new_client(
             &neb_client.raft_client, &neb_client, &neb_server.meta
-        ) {
-            Ok(container) => container,
-            Err(e) => return Err(MorpheusServerError::InitSchemaError(e))
-        };
+        ).map_err(MorpheusServerError::InitSchemaError)?;
         let graph = Arc::new(Graph::new(&schema_container, &neb_client)
             .map_err(MorpheusServerError::InitSchemaError)?);
         Ok(Arc::new(MorpheusServer {
